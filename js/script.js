ac70aea8907f484ee4c14316d199d1275d623b86
Ilham Wahabi <wahabiputra@gmail.com>
2018-04-30 22:15:31 +0700

    Fixed bottom nav

--- js/script.js
+++ js/script.js
@@ -2,7 +2,15 @@ const navbar = document.getElementById('navbar');
 
 window.onscroll = function() {
   const scrollUntilFooter = window.innerHeight * 4;
-  if (window.scrollY >= window.innerHeight * 0.75 && window.scrollY <= scrollUntilFooter) {
+  var minScroll = window.innerHeight * 0.75;
+
+  if (window.innerWidth <= 720) {
+    minScroll = window.innerHeight * 0.25;
+  }
+
+  console.log(minScroll)
+
+  if (window.scrollY >= minScroll && window.scrollY <= scrollUntilFooter) {
     navbar.classList.add('navbar__colored');
   } else {
     navbar.classList.remove('navbar__colored');
