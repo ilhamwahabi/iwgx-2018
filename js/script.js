4b9063eb30200384053c46dc00e3da720cedbf86
Ilham Wahabi <wahabiputra@gmail.com>
2018-04-30 23:40:41 +0700

    Nav hide in bottom

--- js/script.js
+++ js/script.js
@@ -1,20 +1,16 @@
 const navbar = document.getElementById('navbar');
 
 window.onscroll = function() {
-  const scrollUntilFooter = window.innerHeight * 4;
   var minScroll = window.innerHeight * 0.75;
 
   if (window.innerWidth <= 720) {
     minScroll = window.innerHeight * 0.25;
   }
 
-  var element = event.target;
-  if (element.scrollHeight - element.scrollTop === element.clientHeight)
-  {
-      console.log('scrolled');
-  }
-
-  if (window.scrollY >= minScroll && window.scrollY <= scrollUntilFooter) {
+  if (
+    window.scrollY >= minScroll && 
+    (window.innerHeight + window.scrollY) < document.body.offsetHeight
+  ) {
     navbar.classList.add('navbar__colored');
   } else {
     navbar.classList.remove('navbar__colored');
