60379001e3c5abb2c71d3b3ad4437ef05e534710
Ilham Wahabi <wahabiputra@gmail.com>
2018-04-30 21:24:30 +0700

    Nice desktop navbar

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 const navbar = document.getElementById('navbar');
 
 window.onscroll = function() {
-  if (window.scrollY >= window.innerHeight * 0.75) {
+  const scrollUntilFooter = window.innerHeight * 4;
+  if (window.scrollY >= window.innerHeight * 0.75 && window.scrollY <= scrollUntilFooter) {
     navbar.classList.add('navbar__colored');
   } else {
     navbar.classList.remove('navbar__colored');
